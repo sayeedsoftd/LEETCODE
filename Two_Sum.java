9b47fce7a4717465b7cc833b0cbb51b83480fa64
Abu Sayeed <153213740+sayeedsoftd@users.noreply.github.com>
2024-06-01 06:41:35 -0700

    Add files via upload

---
+++ Two_Sum.java
@@ -0,0 +1,25 @@
+package MyJavaProject.LetCode;
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class Two_Sum {
+    public static void main(String[] args) {
+        int[] nums={2,7,11,15};
+        int target=9;
+        System.out.println(Arrays.toString((twoSum(nums,target))));
+    }
+    public static int[] twoSum(int[] nums,int target ){
+        for (int i=0;i<=nums.length;i++)
+        {
+         for (int j=i+1;j<=nums.length;j++)
+         {
+             if (nums[i] + nums[j]==target) {
+                 return new int[] {i,j};
+             }
+         }
+
+        }
+        return nums;
+    }
+
+}
